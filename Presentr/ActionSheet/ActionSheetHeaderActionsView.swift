c82c662554e709fd25310c500b233f809fff9020
Giorgio Fiderio <giorgio@Mac-mini-di-Giorgio.local>
2018-07-16 16:33:06 +0200

    bug fix

--- Presentr/ActionSheet/ActionSheetHeaderActionsView.swift
+++ Presentr/ActionSheet/ActionSheetHeaderActionsView.swift
@@ -13,30 +13,41 @@ class ActionSheetHeaderActionsView: AutoSizingStackView {
     @IBOutlet private weak var actionsView: UIView!
     @IBOutlet private var headerHeightConstraint: NSLayoutConstraint!
     
+    private let priority255 = UILayoutPriority(rawValue: 255)
     private let priority253 = UILayoutPriority(rawValue: 253)
     private let priority251 = UILayoutPriority(rawValue: 251)
-    private let priority255 = UILayoutPriority(rawValue: 255)
+    private let priority258 = UILayoutPriority(rawValue: 258)
+    private let priority260 = UILayoutPriority(rawValue: 260)
     
-     override func layoutSubviews() {
+    private let priority755 = UILayoutPriority(rawValue: 755)
+    private let priority753 = UILayoutPriority(rawValue: 753)
+    private let priority751 = UILayoutPriority(rawValue: 751)
+    private let priority758 = UILayoutPriority(rawValue: 758)
+    private let priority760 = UILayoutPriority(rawValue: 760)
+    
+    override func layoutSubviews() {
         super.layoutSubviews()
         switch (actionsView.intrinsicContentSize.height, headerView.intrinsicContentSize.height) {
         case let (x, y) where x < frame.height / 2 && y > frame.height / 2:
-            actionsView.setContentHuggingPriority(priority253, for: .vertical)
-            headerView.setContentHuggingPriority(priority251, for: .vertical)
-            actionsView.setContentCompressionResistancePriority(priority255, for: .vertical)
-            headerView.setContentCompressionResistancePriority(priority253, for: .vertical)
+            actionsView.setContentHuggingPriority(priority260, for: .vertical)
+            headerView.setContentHuggingPriority(priority258, for: .vertical)
+            actionsView.setContentCompressionResistancePriority(priority260, for: .vertical)
+            headerView.setContentCompressionResistancePriority(priority258, for: .vertical)
         case let (x, y) where y < frame.height / 2 && x > frame.height / 2:
-            headerView.setContentHuggingPriority(priority253, for: .vertical)
+            headerView.setContentHuggingPriority(priority255, for: .vertical)
             actionsView.setContentHuggingPriority(priority251, for: .vertical)
             headerView.setContentCompressionResistancePriority(priority255, for: .vertical)
-            actionsView.setContentCompressionResistancePriority(priority253, for: .vertical)
+            actionsView.setContentCompressionResistancePriority(priority251, for: .vertical)
         case let (x, y) where x > frame.height / 2 && y > frame.height / 2:
-            headerView.setContentHuggingPriority(priority253, for: .vertical)
+            headerView.setContentHuggingPriority(priority255, for: .vertical)
             actionsView.setContentHuggingPriority(priority251, for: .vertical)
             headerView.setContentCompressionResistancePriority(priority255, for: .vertical)
-            actionsView.setContentCompressionResistancePriority(priority253, for: .vertical)
+            actionsView.setContentCompressionResistancePriority(priority251, for: .vertical)
         default:
-            break
+            actionsView.setContentHuggingPriority(priority260, for: .vertical)
+            headerView.setContentHuggingPriority(priority258, for: .vertical)
+            actionsView.setContentCompressionResistancePriority(priority260, for: .vertical)
+            headerView.setContentCompressionResistancePriority(priority258, for: .vertical)
         }
     }
 }
