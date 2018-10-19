59508d001548524ec1ff76891d03d777c2fded1a
Giorgio Fiderio <giorgio.fiderio@staff.aruba.it>
2018-10-19 10:40:10 +0200

    fixed constraint problem

--- Presentr/ActionSheet/ActionSheetHeaderActionsView.swift
+++ Presentr/ActionSheet/ActionSheetHeaderActionsView.swift
@@ -20,6 +20,11 @@ class ActionSheetHeaderActionsView: AutoSizingStackView {
     override func layoutSubviews() {
         super.layoutSubviews()
         switch (actionsView.intrinsicContentSize.height, headerView.intrinsicContentSize.height) {
+        case let (x, y) where x + y < frame.height:
+            actionsView.setContentHuggingPriority(priority258, for: .vertical)
+            headerView.setContentHuggingPriority(priority260, for: .vertical)
+            actionsView.setContentCompressionResistancePriority(priority258, for: .vertical)
+            headerView.setContentCompressionResistancePriority(priority260, for: .vertical)
         case let (x, y) where x < frame.height / 2 && y > frame.height / 2:
             actionsView.setContentHuggingPriority(priority260, for: .vertical)
             headerView.setContentHuggingPriority(priority258, for: .vertical)
@@ -36,10 +41,10 @@ class ActionSheetHeaderActionsView: AutoSizingStackView {
             headerView.setContentCompressionResistancePriority(priority255, for: .vertical)
             actionsView.setContentCompressionResistancePriority(priority251, for: .vertical)
         default:
-            actionsView.setContentHuggingPriority(priority260, for: .vertical)
-            headerView.setContentHuggingPriority(priority258, for: .vertical)
-            actionsView.setContentCompressionResistancePriority(priority260, for: .vertical)
-            headerView.setContentCompressionResistancePriority(priority258, for: .vertical)
+            actionsView.setContentHuggingPriority(priority258, for: .vertical)
+            headerView.setContentHuggingPriority(priority260, for: .vertical)
+            actionsView.setContentCompressionResistancePriority(priority258, for: .vertical)
+            headerView.setContentCompressionResistancePriority(priority260, for: .vertical)
         }
     }
 }
